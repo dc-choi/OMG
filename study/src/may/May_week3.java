58ccc9bd546986d9d1eb390a15b2261aabee19dc
dc-choi <57163578+dc-choi@users.noreply.github.com>
2020-05-18 22:58:01 +0900

    2번 문제 해결

--- study/src/may/May_week3.java
+++ study/src/may/May_week3.java
@@ -1,6 +1,7 @@
 package may;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class May_week3 {
@@ -9,12 +10,15 @@ public class May_week3 {
 		May_week3 m = new May_week3();
 		// 1. Scanner 클래스를 사용하여 6개 학점('A', 'B', 'C', 'D', 'F')을 문자로 입력받아 ArrayList에 저장하고,
 		// ArrayList를 검색하여 학점을 점수(A=4.0, B=3.0, C=2.0, D=1.0, F=0)로 변환하여 평균을 출력하는 프로그램을 작성하라.
-		m.AVG();
+		// m.AVG();
+		
 		// 2. "그만"이 입력될 때까지 나라 이름과 인구를 입력받아 저장하고, 다시 나라 이름을 입력받아 인구를 출력하는 프로그램을 작성하라. 다음 해시맵을 이용하라.
+		// m.STOP();
 		
 		// 3. 하나의 학생 정보를 나타내는 Student 클래스에는 이름, 학과, 학번, 학점 평균을 저장하는 필드가 있다.
 		// (1) 학생마다 Student 객체를 생성하고 4명의 학생 정보를 ArrayList<Student> 컬렉션에 저장한 후에,
 		// ArrayList<Student>의 모든 학생(4명) 정보를 출력하고 학생이름을 입력받아 해당 학생의 학점 평균을 출력하는 프로그램을 작성하라.
+		
 		
 		// 4.정렬되지 않은 학생들의 임의의 점수를 입력하여 석차를 계산하는 프로그램을 작성하시오. 점수는 동점이 있을 수 있으며, 이러한 경우 같은 석차로 처리한다.
 		// 예를 들어 5명의 점수 100, 90, 76, 60, 90이 입력되었다면 석차는 2등이 2명이고 3등은 없다.
@@ -56,5 +60,29 @@ public class May_week3 {
 		// 과목 총합의 평균을 구한다.
 		result = score / scoreList.size();
 		System.out.println("총 점수의 평균은 : " + result);
+	}
+	public void STOP() {
+		String cName = "";
+		String cName2 = "";
+		int cNo = 0;
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		
+		while(true) {
+			System.out.println("나라 이름과 인구수를 입력하시오 (입력을 중지하려면 '그만'을 치시오)");
+			cName = s.next();
+			if(cName.equals("그만")) {
+				break;
+			}
+			cNo = s.nextInt();
+			map.put(cName, cNo);
+		}
+		while(true) {
+			System.out.println("나라 이름을 입력하시오 (출력을 중지하려면 '그만'을 치시오)");
+			cName2 = s.next();
+			if(cName2.equals("그만")) {
+				break;
+			}
+			System.out.println(map.getOrDefault(cName2, cNo));
+		}
 	}
 }
